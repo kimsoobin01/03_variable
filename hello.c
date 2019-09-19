df4625194fe053f7e3fb83819318a6bc0f731f0b
kimsoobin01 <55084058+kimsoobin01@users.noreply.github.com>
2019-09-19 16:13:00 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,17 +3,19 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main(void)  {
+
+	int input_int;
+	float input_float;
 	
-	int x;
-	short y;
-	float z;
-	double r;
+	printf("enter a integer : ");
+	scanf("%d", &input_int);
+		
+	printf("enter a float : ");
+	scanf("%f", &input_float);
 	
-	printf("byie of int : %d\n, sizeof(x)");
-	printf("byie of short : %d\n, sizeof(y)");
-	printf("byie of float : %d\n, sizeof(z)");
-	printf("byie of double : %d\n, sizeof(r)");
+	printf("integer : %d, float : %f", input_int, input_float);
+	
 	
 	return 0;
 }
